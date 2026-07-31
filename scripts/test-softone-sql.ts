26bd99f2bda97b6ebb29b7ce0eb14c602c2c316e
Konstantinos Tsouflias <ktsouflias@primeltd.com>
2026-07-31 19:14:37 +0300

    fix: report SoftOne connection errors

--- scripts/test-softone-sql.ts
+++ scripts/test-softone-sql.ts
@@ -1,12 +1,12 @@
 import "dotenv/config";
-import { testSoftOneSqlConnection } from "../server/lib/softoneSql";
+import { softOneSqlError, testSoftOneSqlConnection } from "../server/lib/softoneSql";
 
 async function main() {
   await testSoftOneSqlConnection();
   console.log("SoftOne SQL connection succeeded.");
 }
 
-main().catch(() => {
-  console.error("SoftOne SQL connection failed.");
+main().catch(error => {
+  console.error(softOneSqlError(error, "connection test"));
   process.exitCode = 1;
 });
